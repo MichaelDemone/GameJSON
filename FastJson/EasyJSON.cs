35be7619929a5b4ea0524e8756c20e8429fb2265
Michael Demone <m_d_emone@hotmail.com>
2021-12-27 10:47:15 -0800

    Wrote EasyJSON
    Improved on FastJSON and wrote EasyJSON with tests. Moved over to Unity to profiling and testing, so program is now a monobehavior. Removed all references to other files. Ideally, folks can just drag and drop these files into their project.

---
+++ FastJson/EasyJSON.cs
@@ -0,0 +1,190 @@
+using System;
+using System.Collections;
+
+namespace FastJson
+{
+    public class EasyJSON 
+    {
+        public static string Serialize(object obj)
+        {
+            FastJson.FastJSONWriter writer = new FastJson.FastJSONWriter();
+            Serialize(obj, writer);
+            return writer.GetJSON();
+        }
+
+        public static void Serialize(object objVal, FastJSONWriter writer) {
+
+            if(objVal == null) 
+            {
+                writer.RawWrite("null");
+            }
+            else if (objVal is bool bo) 
+            {
+                writer.RawWrite(bo);
+            }
+            else if (objVal is string str) {
+                writer.RawWrite(str);
+            }
+            else if (objVal is DateTime dt) {
+                writer.RawWrite(dt.ToString());
+            }
+            else if (objVal is byte by) {
+                writer.RawWrite((double) by);
+            }
+            else if (objVal is sbyte sb) {
+                writer.RawWrite((double) sb);
+            }
+            else if (objVal is char c) {
+                writer.RawWrite("\"");
+                writer.RawWrite(new string(new char[] {c}));
+                writer.RawWrite("\"");
+
+            }
+            else if (objVal is decimal de) {
+                // TODO: Special handling?
+                writer.RawWrite((double)de);
+            }
+            else if (objVal is double d) {
+                writer.RawWrite(d);
+            }
+            else if (objVal is float f) {
+                writer.RawWrite((double) f);
+            }
+            else if (objVal is int i) {
+                writer.RawWrite((double) i);
+            } 
+            else if (objVal is uint ui) {
+                writer.RawWrite((double) ui);
+            }
+            else if (objVal is long l) {
+                // TODO: Loss of precision
+                writer.RawWrite((double) l);
+            }
+            else if (objVal is ulong ul) {
+                writer.RawWrite((double) ul);
+            }
+            else if (objVal is short s) {
+                writer.RawWrite((double) s);
+            }
+            else if (objVal is ushort us) {
+                writer.RawWrite((double) us);
+            }
+            else if (objVal is IEnumerable enumer) 
+            {
+                writer.BeginArray();
+                {
+                    foreach(var val in enumer) {
+                        writer.StartArrayValue();
+                        Serialize(val, writer);
+                        writer.EndArrayValue();
+                    }
+                }
+                writer.EndArray();
+            }
+            else {
+                writer.BeginObject();
+                {
+                    Type objType = objVal.GetType();
+                    foreach(var field in objType.GetFields()) 
+                    {
+                        string name = field.Name;
+                        object fieldVal = field.GetValue(objVal);
+                        writer.BeginProperty(name);
+                        {
+                            Serialize(fieldVal, writer);
+                        }
+                        writer.EndProperty();
+                    }
+                }
+                writer.EndObject();
+            }
+        }
+    
+        public static T Deserialize<T>(string s) {
+            FastJSONReader reader = new FastJSONReader(s);
+            return (T) Deserialize(typeof(T), reader);
+        }
+
+        public static object Deserialize(Type ttype, FastJSONReader reader) {
+
+            if(reader.IsNullToken()) {
+                reader.ConsumeNull();
+                return null;
+            }
+            if (ttype == typeof(bool)) {
+                return reader.ConsumeBoolValue();
+            }
+            else if (ttype == typeof(string)) {
+                return reader.ConsumeStringValue();
+            }
+            else if (ttype == typeof(DateTime)) {
+                return reader.ConsumeDateTime();
+            }
+            else if (ttype == typeof(byte))     return (byte)   reader.ConsumeDoubleValue();
+            else if (ttype == typeof(sbyte))    return (sbyte)  reader.ConsumeDoubleValue();
+            else if (ttype == typeof(float))    return (float)  reader.ConsumeDoubleValue();
+            else if (ttype == typeof(double))   return (double) reader.ConsumeDoubleValue();
+            else if (ttype == typeof(int))      return (int)    reader.ConsumeDoubleValue();
+            else if (ttype == typeof(uint))     return (uint)   reader.ConsumeDoubleValue();
+            else if (ttype == typeof(long))     return (long)   reader.ConsumeDoubleValue();
+            else if (ttype == typeof(ulong))    return (ulong)  reader.ConsumeDoubleValue();
+            else if (ttype == typeof(short))    return (short)  reader.ConsumeDoubleValue();
+            else if (ttype == typeof(ushort))   return (ushort) reader.ConsumeDoubleValue();
+            else if (ttype == typeof(char)) {
+                return reader.ConsumeCharValue();
+            }
+            else if (ttype == typeof(decimal)) {
+                return (decimal) reader.ConsumeDoubleValue();
+            }
+            else {
+                Type listType = ttype.GetInterface(nameof(IList)+"'1");
+                if (ttype.IsArray || listType != null) {
+                    
+                    Type elementType;
+                    if(ttype.IsArray) {
+                        elementType = ttype.GetElementType();
+                    } 
+                    else {
+                        elementType = listType.GetGenericArguments()[0];
+                    }
+
+                    reader.ExpectArrayStart();
+                    var array = Array.CreateInstance(elementType, reader.GetArrayLength());
+                    {
+                        for(int i = 0; !reader.IsAtArrayEnd(); i++) 
+                        {
+                            var deserializedArrayObject = Deserialize(elementType, reader);
+                            array.SetValue(deserializedArrayObject, i);
+                        }
+                    }
+                    reader.ExpectArrayEnd();
+                    return array;
+                }
+                else {
+                    object result = System.Runtime.Serialization.FormatterServices.GetUninitializedObject(ttype);
+                    reader.ExpectObjectStart();
+                    {
+                        System.Reflection.FieldInfo[] fields = ttype.GetFields();
+                        while(!reader.IsAtObjectEnd()) {
+                            bool consumed = false;
+                            foreach(var field in fields) {
+                                if(reader.ConsumeIfProperyNameEquals(field.Name)) {
+                                    UnityEngine.Debug.Log($"Consuming: {field.Name}");
+                                    var fieldValue = Deserialize(field.FieldType, reader);
+                                    field.SetValue(result, fieldValue);
+                                    consumed = true;
+                                }
+                            }
+
+                            if(!consumed) {
+                                reader.ConsumeUnknownValue();
+                            }
+                        }
+                    }
+                    reader.ExpectObjectEnd();
+                    return result;
+                }
+            }
+        }
+    }
+}
---
+++ FastJson/Program.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FastJson
+{
+    class Program : MonoBehaviour
+    {
+        private const string Test = @"
+{
+	""DoubleValueProperty0"" :   142.010,
+	""ValueProperty1"": ""142.010"",
+	""ValueProperty2"": ""142"",
+	""DoubleValueProperty3"": 0.00000123,
+	""DoubleValueProperty4"": 14.452E-23,
+	""DoubleValueProperty5"": 14.452E+23,
+	""DoubleValueProperty6"": 14.452E23,
+	""DoubleValueProperty7"": 14.452e-23,
+	""DoubleValueProperty8"": 14.452e+23,
+	""DoubleValueProperty9"": 14.452e23,
+	""BoolProperty10"": true,
+	""BoolProperty11"": false,
+	""ArrayProperty1"": [
+		""Object"",
+		132,
+		{
+			""InAnArray"": true
+
+        },
+		{
+			""InAnArrayToo"": true
+		}
+	],
+	""ObjectProperty1"": {
+    ""MyNestedObject"": {
+        ""InNestedObject"" : true
+
+        }
+},
+	""ObjectProperty2"": null,
+    ""TotalUnknown"": [],
+    ""TotalUnknown"": [],
+    ""IntArray""  :  [1,2,3,4 , 5 ,78, 9],
+    ""BoolArray""  :  [true, True, TRUE, false, False, FALSE]
+}
+";
+
+        public struct TestStruct
+        {
+            public bool BoolVal;
+            public byte ByteVal;
+            public sbyte SByteVal;
+            public char CharVal;
+            public decimal DecimalVal;
+            public double DoubleVal;
+            public float FloatVal;
+            public int IntVal;
+            public uint UIntVal;
+            public long LongVal;
+            public ulong ULongVal;
+            public short ShortVal;
+            public ushort UShortVal;
+            
+            
+            public double DoubleValueProperty0;
+            public int IntValueProperty0;
+            public short ShortValueProperty0;
+            public float FloatValueProperty0;
+            public string ValueProperty1;
+            public bool BoolProperty10;
+            public bool BoolProperty11;
+            public object[] ArrayProperty1;
+            public int[] IntArray;
+            public bool[] BoolArray;
+            public List<int> ListOfInts;
+            public NestedStruct ObjectProperty1;
+            
+            public NestedStruct[] Structs;
+            public List<NestedStruct> ListOfStructs;
+
+            public NestedClass NestedConcreteClass;
+            public List<NestedClass> ListOfNestedConcreteClass;
+            public NestedClass[] NestedConcreteClassArray;
+
+            public NestedGenericClass<bool> NestedGeneric;
+            public struct NestedStruct
+            {
+                public bool InNestedObject;
+                public int RandomValue;
+            }
+
+            public class NestedClass 
+            {
+                public bool InNestedClass;
+                public string MyString;
+            }
+
+            public class NestedGenericClass<T> 
+            {
+                public T OnlyObject;
+                public bool InGenericClass;
+            }
+
+            public static TestStruct MakeDefault() {
+                TestStruct ts = default;
+                ts.CharVal = 'a';
+                return ts;
+            }
+        }
+
+        public void Start()
+        {
+            FloatTests();
+            ReadTests();
+            WriteTests();
+            ReflectionTests();
+        }
+
+        public void ReadTests() {
+            TestStruct test = default;
+
+            FastJSONReader json = new FastJSONReader(Test);
+
+            json.ExpectObjectStart();
+            while (!json.IsAtObjectEnd()) {
+                if(json.ConsumeIfProperyNameEquals(nameof(test.DoubleValueProperty0)))
+                {
+                    test.DoubleValueProperty0 = json.ConsumeDoubleValue();
+                }
+                else if (json.ConsumeIfProperyNameEquals(nameof(test.ValueProperty1)))
+                {
+                    test.ValueProperty1 = json.ConsumeStringValue();
+                }
+                else if (json.ConsumeIfProperyNameEquals(nameof(test.BoolProperty10)))
+                {
+                    test.BoolProperty10 = json.ConsumeBoolValue();
+                }
+                else if (json.ConsumeIfProperyNameEquals(nameof(test.BoolProperty11)))
+                {
+                    test.BoolProperty11 = json.ConsumeBoolValue();
+                }
+                else if (json.ConsumeIfProperyNameEquals(nameof(test.ObjectProperty1))) 
+                {
+                    json.ExpectObjectStart(); 
+                    {
+                        json.ConsumeIfProperyNameEquals("MyNestedObject");
+                        json.ExpectObjectStart();
+                        {
+                            json.ConsumeIfProperyNameEquals("InNestedObject");
+                            test.ObjectProperty1.InNestedObject = json.ConsumeBoolValue();
+                        }
+                        json.ExpectObjectEnd();
+                    }
+                    json.ExpectObjectEnd();
+                }
+                else if (json.ConsumeIfProperyNameEquals(nameof(test.IntArray)))
+                {
+                    json.ConsumeUnknownValue();
+                }
+                else
+                {
+                    json.ConsumePropertyName();
+                    json.ConsumeUnknownValue();
+                }
+            }
+            json.ExpectObjectEnd();
+
+            Assert(json.IsDone(), $"Json is not done reading. {json.Position} read out of {json.Json.Length}");
+
+            Assert(test.DoubleValueProperty0 == 142.010, $"{nameof(test.DoubleValueProperty0)}: {test.DoubleValueProperty0} does not equal 142.010");
+            Assert(test.ValueProperty1 == "142.010", $"{nameof(test.ValueProperty1)}: {test.ValueProperty1} does not equal 142.010");
+            Assert(test.BoolProperty10 == true, $"{nameof(test.BoolProperty10)}: {test.BoolProperty10} does not equal true");
+            Assert(test.BoolProperty11 == false, $"{nameof(test.BoolProperty11)}: {test.BoolProperty11} does not equal true");
+            Assert(test.ObjectProperty1.InNestedObject == true, $"{nameof(test.ObjectProperty1.InNestedObject)}: {test.ObjectProperty1.InNestedObject} does not equal true");
+
+            Print("Done!");
+        }
+
+        public void WriteTests() {
+            TestStruct ts = new TestStruct();
+            ts.DoubleValueProperty0 = 142.010;
+            ts.ValueProperty1 = "142.010";
+            ts.ObjectProperty1.InNestedObject = true;
+            ts.IntArray = new int[] {1, 2, 3, 4, 5, 78, 9};
+            ts.BoolProperty10 = false;
+            ts.BoolProperty11 = true;
+
+            FastJSONWriter writer = new FastJSONWriter();
+            writer.BeginObject();
+            {
+                writer.WriteProperty(nameof(ts.DoubleValueProperty0), ts.DoubleValueProperty0);
+                writer.WriteProperty(nameof(ts.ValueProperty1), ts.ValueProperty1);
+                //writer.WriteProperty(nameof(ts.DoubleValueProperty4), ts.DoubleValueProperty4);
+                writer.BeginProperty(nameof(ts.ObjectProperty1));
+                writer.BeginObject();
+                {
+                    writer.WriteProperty(nameof(ts.ObjectProperty1.InNestedObject), true);
+                }
+                writer.EndObject();
+                writer.EndProperty();
+
+                writer.WriteProperty(nameof(ts.BoolProperty10), ts.BoolProperty10);
+                writer.WriteProperty(nameof(ts.BoolProperty11), ts.BoolProperty11);
+
+                writer.BeginProperty(nameof(ts.IntArray));
+                writer.BeginArray();
+                {
+                    foreach(var val in ts.IntArray) {
+                        writer.WriteArrayValue(val);
+                    }
+                }
+                writer.EndArray();
+                writer.EndProperty();
+            }
+            writer.EndObject();
+            Print(writer.GetJSON());
+        }
+
+        public static void FloatTests()
+        {
+            FloatTest("1234.54321");
+            FloatTest("1234.54321e21");
+            FloatTest("1234.54321e+21");
+            FloatTest("1234.54321e-21");
+
+            FloatTest("-1234.54321");
+            FloatTest("-1234.54321e21");
+            FloatTest("-1234.54321e+21");
+            FloatTest("-1234.54321e-21");
+        }
+
+        private static void FloatTest(string testInput) {
+            double actual = InPlaceParsing.ParseDouble(testInput, 0, testInput.Length);
+            double expected = double.Parse(testInput);
+            Assert(Math.Abs(actual - expected) < 0.01d, $"float test: {actual} does not equal {expected}");
+        }
+
+        public void ReflectionTests() 
+        {
+            DefaultReflectionTest();
+            //StringReflectionTest();
+            IntArrayReflectionTest();
+            StructArrays();
+            StructList();
+            ClassArrays();
+            ClassList();
+
+            void DefaultReflectionTest() {
+                TestStruct ts = TestStruct.MakeDefault();
+                ts.BoolProperty10 = true;
+                string s = EasyJSON.Serialize(ts);
+                Print(s);
+                var deserializeRes = EasyJSON.Deserialize<TestStruct>(s);
+                Assert(deserializeRes.BoolProperty10, "Did not properly deserialize");
+            }
+
+            void StringReflectionTest() {
+                TestStruct ts = TestStruct.MakeDefault();
+                string escapeString = "Escape characters lets gooo \"\\ how about \n or maybe \t or how about \r??";
+                ts.ValueProperty1 = escapeString;
+                string s = EasyJSON.Serialize(ts);
+                Print(s);
+                var deserializeRes = EasyJSON.Deserialize<TestStruct>(s);
+                Assert(deserializeRes.ValueProperty1 == ts.ValueProperty1, "Did not properly deserialize");
+            }
+
+            void IntArrayReflectionTest() {
+                TestStruct ts = TestStruct.MakeDefault();
+                ts.IntArray = new int[10];
+                ts.IntArray[0] = 5;
+                ts.IntArray[8] = 10;
+                string s = EasyJSON.Serialize(ts);
+                Print(s);
+                var deserializeRes = EasyJSON.Deserialize<TestStruct>(s);
+                for(int i = 0; i < ts.IntArray.Length; i++) {
+                    Assert(deserializeRes.IntArray[i] == ts.IntArray[i], "Did not properly deserialize");
+                }
+            }
+
+            void StructArrays() {
+                TestStruct ts = TestStruct.MakeDefault();
+                ts.Structs = new TestStruct.NestedStruct[10];
+                TestStruct.NestedStruct ns = ts.Structs[0];
+                ns.RandomValue = 1;
+                ts.Structs[0] = ns;
+                string s = EasyJSON.Serialize(ts);
+                Print(s);
+                var deserializeRes = EasyJSON.Deserialize<TestStruct>(s);
+                Assert(deserializeRes.Structs.Length == 10, "Did not properly deserialize");
+                for(int i = 0; i < ts.Structs.Length; i++) {
+                    Assert(deserializeRes.Structs[i].RandomValue == ts.Structs[i].RandomValue, "Did not properly deserialize");
+                }
+            }
+
+            void StructList() {
+                TestStruct ts = TestStruct.MakeDefault();
+                ts.ListOfStructs = new List<TestStruct.NestedStruct>(10);
+                TestStruct.NestedStruct ns = new TestStruct.NestedStruct();
+                ns.RandomValue = 1;
+                ts.ListOfStructs.Add(ns);
+                string s = EasyJSON.Serialize(ts);
+                Print(s);
+                var deserializeRes = EasyJSON.Deserialize<TestStruct>(s);
+                Assert(deserializeRes.ListOfStructs.Count == 1, "Did not properly deserialize");
+                for(int i = 0; i < ts.ListOfStructs.Count; i++) {
+                    Assert(deserializeRes.ListOfStructs[i].RandomValue == ts.ListOfStructs[i].RandomValue, "Did not properly deserialize");
+                }
+            }
+
+            void ClassArrays() {
+                TestStruct ts = TestStruct.MakeDefault();
+                ts.NestedConcreteClassArray = new TestStruct.NestedClass[10];
+                TestStruct.NestedClass nc = new TestStruct.NestedClass();
+                nc.MyString = "TESTSSSSS";
+                ts.NestedConcreteClassArray[0] = nc;
+                string s = EasyJSON.Serialize(ts);
+                Print(s);
+                var deserializeRes = EasyJSON.Deserialize<TestStruct>(s);
+                Assert(deserializeRes.NestedConcreteClassArray[0].MyString == nc.MyString, "Did not properly deserialize");
+                for(int i = 1; i < ts.Structs.Length; i++) {
+                    Assert(deserializeRes.NestedConcreteClassArray[i] == null, "Did not properly deserialize");
+                }
+            }
+
+            void ClassList() {
+                TestStruct ts = TestStruct.MakeDefault();
+                ts.ListOfNestedConcreteClass = new List<TestStruct.NestedClass>(10);
+                var ns = new TestStruct.NestedClass();
+                ns.MyString = "TEST2";
+                ts.ListOfNestedConcreteClass.Add(ns);
+                string s = EasyJSON.Serialize(ts);
+                Print(s);
+                var deserializeRes = EasyJSON.Deserialize<TestStruct>(s);
+                Assert(deserializeRes.ListOfNestedConcreteClass.Count == 1, "Did not properly deserialize");
+                for(int i = 0; i < ts.ListOfNestedConcreteClass.Count; i++) {
+                    Assert(deserializeRes.ListOfNestedConcreteClass[i].MyString == ts.ListOfNestedConcreteClass[i].MyString, "Did not properly deserialize");
+                }
+            }
+            
+        }
+
+        private static void Assert(bool assertion, string message) {
+            UnityEngine.Debug.Assert(assertion, message);
+        }
+
+        private static void Print(string message) {
+            print(message);
+        }
+    }
+}
